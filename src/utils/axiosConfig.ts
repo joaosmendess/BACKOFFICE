c2233194d3afe19f867af95befcdc4d0972cae4d
joaosmendess <joaosilva0721@gmail.com>
2024-06-28 15:59:21 -0300

    first commit

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8989/api',
+  baseURL: 'http://10.1.1.242:4848/api/',
   headers: {
     'Content-Type': 'application/json',
   },
